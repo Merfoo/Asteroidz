b23f189df9e50981c25d20c9caa390802974aca0
Merfoo <fauzimerfoo@gmail.com>
2013-04-11 09:46:02 -0700

    Asteroidz 5.7
    Fixed background not being cleared for the endgame text.

--- js/single.js
+++ js/single.js
@@ -104,6 +104,9 @@ function gameLoopSingle()
     
     writeMessage(m_iTextAlign.left, m_iTextAlign.top, m_iFontSize.medium, "Time: " + m_iTime.current, m_iTime.color);
     writeMessage(m_iTextAlign.left + 333, m_iTextAlign.top, m_iFontSize.medium, "Score: " + m_iScores.one, m_iTime.color);
+    
+    if(m_bGameStatus.lost)
+        endGameSingle();
 }
 
 // Stops loop
@@ -135,8 +138,7 @@ function endGameSingle()
     m_bGameStatus.lost = true;
     m_iScores.list.push(m_iScores.one = floor(m_iScores.one + m_iTime.current * m_iTime.multiplyer));
     m_iScores.list = order(m_iScores.list, true);
-    m_CanvasMain.fillStyle = "black";
-    m_CanvasMain.fillRect(textRect.x, textRect.y, textRect.width, textRect.height);
+    m_CanvasMain.clearRect(textRect.x, textRect.y, textRect.width, textRect.height);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle, m_iFontSize.big, "You Lost!!!", m_iScores.color);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle + 75, m_iFontSize.small, "Time Survived: " + m_iTime.current, m_iScores.color);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle + 100, m_iFontSize.small, "Score: " + m_iScores.one + ",  Highest: " + m_iScores.list[0], m_iScores.color);
