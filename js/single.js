8da59d947a8d6d07125409babd54cd0e3d483fa2
Merfoo <fauzimerfoo@gmail.com>
2013-04-12 08:33:09 -0700

    Asteroidz 6.5
    Endgame text back to normal, not red like previous one.

--- js/single.js
+++ js/single.js
@@ -146,9 +146,7 @@ function endGameSingle()
     m_bGameStatus.lost = true;
     m_iScores.list.push(m_iScores.one = floor(m_iScores.one + m_iTime.current * m_iTime.multiplyer));
     m_iScores.list = order(m_iScores.list, true);
-    //m_CanvasMain.clearRect(textRect.x, textRect.y, textRect.width, textRect.height);
-    m_CanvasMain.fillStyle = "red";
-    m_CanvasMain.fillRect(textRect.x, textRect.y, textRect.width, textRect.height);
+    m_CanvasMain.clearRect(textRect.x, textRect.y, textRect.width, textRect.height);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle, m_iFontSize.big, "You Lost!!!", m_iScores.color);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle + 75, m_iFontSize.small, "Time Survived: " + m_iTime.current, m_iScores.color);
     writeMessage(m_iTextAlign.center, m_iTextAlign.middle + 100, m_iFontSize.small, "Score: " + m_iScores.one + ",  Highest: " + m_iScores.list[0], m_iScores.color);
